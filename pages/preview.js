0875aaa495e621eed5da0d44d11bce47508aeb8a
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 21:45:06 +0300

    Create preview.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ pages/preview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PreviewPage = () => {
+  return (
+    <div className="page">
+      <h1>معاينة المشروع</h1>
+      <p>يمكنك هنا مشاهدة المشروع قبل النشر النهائي.</p>
+    </div>
+  )
+}
+
+export default PreviewPage
