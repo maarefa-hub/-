19dbff0aa0c48820b3b56246ca9d142afdbbccc5
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 22:27:53 +0300

    Create index.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ store/index.js
@@ -0,0 +1,11 @@
+import featureStore from './featureStore.js'
+import projectStore from './projectStore.js'
+import userStore from './userStore.js'
+import settingsStore from './settingsStore.js'
+
+export default {
+  featureStore,
+  projectStore,
+  userStore,
+  settingsStore
+}
