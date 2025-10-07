77db7f020687ef85698a0f6ab3fd77621994beae
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 21:54:56 +0300

    Create projectData.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ projects/projectData.js
@@ -0,0 +1,16 @@
+const projectData = [
+  {
+    id: 1,
+    title: 'مشروع المعرفة',
+    description: 'منصة ذكية لتحويل الأفكار إلى مشاريع قابلة للتنفيذ',
+    status: 'جاهز للنشر'
+  },
+  {
+    id: 2,
+    title: 'مساعد المحتوى',
+    description: 'أداة لإنشاء محتوى تلقائي باللغة العربية',
+    status: 'قيد التطوير'
+  }
+]
+
+export default projectData
