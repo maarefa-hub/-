85eaf9d846fa84a8171ea9ff1d1735d98f36c0a7
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 22:47:21 +0300

    Create Footer.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© جميع الحقوق محفوظة - منصة المعرفة</p>
+    </footer>
+  )
+}
+
+export default Footer
