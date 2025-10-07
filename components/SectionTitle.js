66d117f213b2106891f85b5e6d6f3c800f2c2001
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 18:57:37 +0300

    Create SectionTitle.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ components/SectionTitle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SectionTitle({ title }) {
+  return (
+    <h2 className="text-2xl font-bold text-gray-800 mb-4 text-center">
+      {title}
+    </h2>
+  )
+}
