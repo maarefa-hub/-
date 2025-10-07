4d543bc2313675d13457a3b5ced7d79cf0442868
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 18:36:03 +0300

    Create branding.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ config/branding.js
@@ -0,0 +1,8 @@
+export const branding = {
+  logo: "/assets/logo.svg",
+  favicon: "/assets/favicon.png",
+  slogan: "منصة ذكية لتحويل الأفكار إلى واقع",
+  language: "العربية",
+  direction: "rtl",
+  repository: "https://github.com/maarefa/maarefa-hub"
+}
