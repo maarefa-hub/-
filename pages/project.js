4cc337aeab2cf0212b9ee62b442157f31ec98c6b
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 21:42:46 +0300

    Create project.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ pages/project.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <div className="page">
+      <h1>تفاصيل المشروع</h1>
+      <p>هنا يتم عرض معلومات المشروع المختار، مثل الاسم والوصف والمحتوى.</p>
+    </div>
+  )
+}
+
+export default ProjectPage
