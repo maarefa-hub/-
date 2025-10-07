9b8eb9e01faf8f179d36fc3b402e37d4a4691bab
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 21:45:53 +0300

    Create notfound.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ pages/notfound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div className="page">
+      <h1>404 - الصفحة غير موجودة</h1>
+      <p>عذرًا، الصفحة التي تبحث عنها غير متوفرة.</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
