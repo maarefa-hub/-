751a9b06bb51fb79f406ea4b53fe6a04a19b7e4e
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 18:31:10 +0300

    Create site.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ config/site.js
@@ -0,0 +1,8 @@
+export const siteName = ""
+
+export const repository = "https://github.com/maarefa/maarefa-hub"
+
+export const contactEmails = [
+  "maswuwl@hotmail.com",
+  "K774468815K@gmail.com"
+]
