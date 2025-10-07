2e249f6096e5e6b97f7bcbe2d2feeba027f153d2
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 18:32:08 +0300

    Create colors.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ config/colors.js
@@ -0,0 +1,13 @@
+export const userRolesColors = {
+  مسؤول: 'gold',
+  نشط: 'blue',
+  مبتدئ: 'gray',
+  مميز: 'green'
+}
+
+export const themeColors = {
+  background: '#f5f6fa',
+  primary: '#4a90e2',
+  accent: '#7ed6df',
+  text: '#2f3640'
+}
