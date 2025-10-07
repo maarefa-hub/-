4d9c0fcdd01718b074b7967294595a493bf5cb1a
خالد المنتصر <k774468815k@gmail.com>
2025-10-07 18:33:19 +0300

    Create routes.js
    Signed-off-by: خالد المنتصر <k774468815k@gmail.com>

---
+++ config/routes.js
@@ -0,0 +1,9 @@
+export const routes = {
+  dashboard: "/dashboard",
+  store: "/store",
+  login: "/login",
+  chat: "/chat",
+  admin: "/admin-control",
+  preview: "/preview",
+  settings: "/settings"
+}
